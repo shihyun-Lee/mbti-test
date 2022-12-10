03142a45bab15defdb0df26f63892b4bf9942fb5
shihyun-Lee <jeenii22@naver.com>
2022-12-11 00:41:13 +0900

    fix(test.css): color change

--- js/test.js
+++ js/test.js
@@ -90,20 +90,20 @@ const test = new Vue({
             if(this.qna[0].r=='혼자 푼다'){
                 if(this.qna[3].r=='예'){
                     this.resultImage = 'java.png';
-                    this.resultLang = 'java';
+                    this.resultLang = 'Java';
                     this.resultMbti = '#집콕 #미래 #계획 #책임감 #목표';
                     this.resultContent = '요새 개발자라면 자바 안 배우는 분들 없죠. 쉬운 편은 아니지만 늘 인기가 많은 언어입니다. 자바가 츤데레인 이유! 눈앞에 보이는 화면 말고, 무심한 듯 시크하게 뒤에서 지원해주는 프로그램을 만드는 데 유용하기 때문이에요'
                 }
                 else{
                     this.resultImage = 'c++.png';
-                    this.resultLang = 'c++';
+                    this.resultLang = 'C++';
                     this.resultMbti = '#집콕 #미래 #계획 #책임감 #목표';
                     this.resultContent ='C++은 C언어의 단점을 보완한 언어입니다. 이름 그대로 C를 ++ 했다고 볼 수 있죠. 큼직큼직한 응용프로그램을 만들 수 있어 대형 게임들을 만들 때도 사용해요. 구글, 크롬, 파이어폭스, 마이크로소프트 워드 등은 모두 C++로 개발했습니다. C++이 행동 대장일만 하죠?'
                 }
             }else{
                 if(this.qna[3].r=='예'){
                     this.resultImage = 'python.png';
-                    this.resultLang = 'python';
+                    this.resultLang = 'Python';
                     this.resultMbti = '#에너제틱 #아이디어 #계획 #적응 #데이터';
                     this.resultContent ='가장 기초적인 언어이고 최근 들어 많은 사람들이 사용해요사람에 가까운 언어라 배우기 쉬워요 이것은 웹 사이트, 서비스 개발, 데이터 분석 등 다양하게 사용돼요'
 
